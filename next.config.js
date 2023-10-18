60c2c577ab52fdcc972bee7f6146779800644be1
Marija Najdova <mnajdova@gmail.com>
2023-10-18 14:52:01 +0200

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: ['@mui/system', '@mui/material', '@mui/styled-engine', '@mui/styled-engine-sc'],
+  compiler: {
+    styledComponents: {
+      ssr: true,
+      topLevelImportPaths:
+      [
+        "@mui/material",
+        "@mui/material/*",
+        "@mui/system",
+        "@mui/styled-engine-sc",
+        // This maps both the imports in ./src/Typography and the imports in mui
+        '.*/styled$',
+      ]
+    }
+  },
+  webpack: (config, { isServer }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@mui/styled-engine": "@mui/styled-engine-sc",
+    };
+
+    if (isServer) {
+      config.resolve.mainFields = ["module", "main"];
+    }
+
+    return config;
+  }
+}
 
 module.exports = nextConfig
